1549519ce6947998153dcf40c6ca5e51c9844153
Kurulko <kurul300013@gmail.com>
2026-04-23 08:56:23 +0300

    added "Greeting" message

--- AzureAppKurulko2/Pages/Index.cshtml.cs
+++ AzureAppKurulko2/Pages/Index.cshtml.cs
@@ -5,9 +5,15 @@ namespace AzureAppKurulko2.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IConfiguration _configuration;
+        public IndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _configuration["Greeting"] ?? "it's empty";
         }
     }
 }
